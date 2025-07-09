7fd2b3d16d8d7094bc68759ce0495dd5d38a3db4
unknown <lowanagi08@gmail.com>
2025-07-09 23:44:17 +0700

    new look

---
+++ sidebar.js
@@ -0,0 +1,13 @@
+
+const toggleBtn = document.getElementById('sidebar-toggle');
+const sidebarClose = document.getElementById('sidebar-close');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('hidden');
+    toggleBtn.classList.toggle('hidden');
+});
+sidebarClose.addEventListener('click',()=>{
+    sidebar.classList.toggle('hidden');
+    toggleBtn.classList.toggle('hidden');
+});
