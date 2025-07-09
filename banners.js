856147dada8d7e79948392ab5daa51b0e7ec9f59
NagiEight <lowanagi08@gmail.com>
2025-07-10 00:06:58 +0700

    seperate scripts from banners.html

---
+++ banners.js
@@ -0,0 +1,13 @@
+    const gridBtn = document.getElementById('grid-view');
+const listBtn = document.getElementById('list-view');
+const gallery = document.getElementById('gallery');
+
+gridBtn.addEventListener('click', () => {
+  gallery.classList.remove('flex', 'flex-col');
+  gallery.classList.add('grid', 'grid-cols-2', 'md:grid-cols-4');
+});
+
+listBtn.addEventListener('click', () => {
+  gallery.classList.remove('grid', 'grid-cols-2', 'md:grid-cols-4');
+  gallery.classList.add('flex', 'flex-col');
+});
