7c0b75784e8b9b3d96b4ed77e9e02d5422822e17
Zizel <danbua999@gmail.com>
2025-07-12 23:38:57 +0700

    test

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,13 +1,14 @@
-const BannerSection = () => (
-  <div className="grid-cols-2 p-4 flex">
-    <div className="grid-rows-2">
-      <img
-        src="./data/banners/art/[Carnival]_Cloudtop_Lucid_Dreams.png"
-        alt="banner-image"
-        className="rounded-lg border border-gray-700"
-      />
+const BannerSection = (bannerID, bannerName, bannerArtPath) => (
+    <div id="gallery" class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4">
+        <div class="bg-gray-800 rounded-lg overflow-hidden shadow hover:shadow-lg transition">
+            <img src={bannerArtPath} alt={bannerID} class="w-full h-48 object-cover"/>
+            <div class="p-4">
+                <h3 class="text-white font-semibold text-lg">
+                    {bannerName}
+                </h3>
+            </div>
+        </div>
     </div>
-  </div>
 );
 
 export default BannerSection;
