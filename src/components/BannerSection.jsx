5106e677ebfceb4a9ddb28f15a84976480070689
NagiEight <lowanagi08@gmail.com>
2025-07-14 04:15:16 +0700

    responsive ui for gacha page

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,15 +1,12 @@
-const BannerSection = ({bannerID}) => (
-  <div className="grid-cols-2 p-4 flex">
-    <div className="relative grid-rows-2">
-      <img
-        src={`/data/banners/art/${bannerID}.png`}
-        alt={bannerID}
-        className="rounded-lg border border-gray-700"
-      />
-      <div className="absolute bottom-3 left-2 bg-gray-900/70 text-white text-xs px-3 py-1 rounded width-20">
-        Available operators:  Exusiai, SilverAsh, Eyjafjalla, Siege, Saria, Ch'en, Hoshiguma, Nightingale, Shining, Ifrit, Texas, Lappland, Amiya, Myrtle, Specter, Blue Poison, Meteorite, Silence, Warfarin, Zima, Ptilopsis, Liskarm, Red, Glaucus, Manticore, Croissant, Mayer, Earthspirit, Beehunter, Gummy
-
-      </div>
+const BannerSection = ({ bannerID }) => (
+  <div className="grid-cols-3 p-4 lg:flex">
+    <img
+      src={`/data/banners/art/${bannerID}.png`}
+      alt={bannerID}
+      className="rounded-lg border border-gray-700 w-full"
+    />
+    <div className="lg:absolute top-9 left-7 bg-gray-900/70 text-white text-xs px-3 py-1 rounded lg:w-3/5 h-auto">
+      Available operators:  Exusiai, SilverAsh, Eyjafjalla, Siege, Saria, Ch'en, Hoshiguma, Nightingale, Shining, Ifrit, Texas, Lappland, Amiya, Myrtle, Specter, Blue Poison, Meteorite, Silence, Warfarin, Zima, Ptilopsis, Liskarm, Red, Glaucus, Manticore, Croissant, Mayer, Earthspirit, Beehunter, Gummy
     </div>
   </div>
 );
--- src/pages/Gachapage.jsx
+++ src/pages/Gachapage.jsx
@@ -1,18 +1,22 @@
 import { useParams } from 'react-router-dom';
 import BannerSection from '../components/BannerSection';
+import PullButtons from '../components/PullButtons.jsx';
 
-const GachaPage = () => {
+const Gachapage = () => {
   const { bannerID } = useParams();
   return (
 
     <div>
       <main>
         bannerID = {bannerID}
-        <BannerSection bannerID={bannerID} />
+        <div className='relative lg:w-2/3'>
+          <BannerSection bannerID={bannerID} />
+          <PullButtons></PullButtons>
+        </div>
       </main>
     </div>
   );
 }
 
 
-export default GachaPage;
+export default Gachapage;
